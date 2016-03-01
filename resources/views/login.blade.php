b07334fa0360c9702c4e116c39c2b56fa92109e4
Kyle Ramey <codefusion1@gmail.com>
2016-03-01 14:05:09 -0400

    Login form now at /admin with error state and wave button

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,8 +2,33 @@
 
 @section('title', 'Login')
 
+@section('styles')
+    <link href="{{ URL::asset('css/login.css') }}" rel="stylesheet" type="text/css" media="screen,projection"/>
+@endsection
+
 @section('body')
-    <div class="valign-wrapper">
-        <div class="valign"></div>
+    <div class="valign-wrapper login-wrapper">
+        <div class="valign center-block">
+            <div class="card z-depth-4">
+                <div class="card-content">
+                    <span class="card-title">Admin Login</span>
+                    <form>
+                        <div class="input-field">
+                            <input type="email" id="email" name="email" class="validate"/>
+                            <label for="email">Email Address</label>
+                        </div>
+                        <div class="input-field">
+                            <input type="password" id="password" name="password"/>
+                            <label for="password">Password</label>
+                        </div>
+                        <input type="checkbox" id="rememberme" class="filled-in" name="rememberme"/>
+                        <label for="rememberme">Remember Me</label>
+                    </form>
+                </div>
+                <div class="card-action right-align block-buttons">
+                    <a href="#" class="teal-text text-lighten1 waves-effect">Login</a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/partials/skeleton.blade.php
+++ resources/views/partials/skeleton.blade.php
@@ -1,8 +1,9 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <title>@yield('title') - NS Sandbox App Admin</title>
     <link href="https://fonts.googleapis.com/icon?family-Material+Icons" rel="stylesheet"/>
-    <link href="{{ URL::asset('css/materialize.min.css'); }}" rel="stylesheet" type="text/css" media="screen,projection"/>
+    <link href="{{ URL::asset('css/materialize.css') }}" rel="stylesheet" type="text/css" media="screen,projection"/>
     @yield('styles')
     @yield('scripts')
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -10,6 +11,6 @@
 <body>
     @yield('body')
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="{{ URL::asset('js/materialize.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
 </body>
 </html>
