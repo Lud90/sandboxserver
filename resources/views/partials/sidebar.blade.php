b65d8a0f07d725774b0ab78af0ca80070ceba556
Cole DeMan <cl422670@dal.ca>
2016-03-08 17:08:21 -0400

    added comments to sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,13 +1,16 @@
 <!--
-    This is the side bar that will accompany all admin pages
+    This is the side bar that will accompany all admin pages.
  -->
 <div class="col s3">
     <ul class="collection collapsible" data-collapsible="accordion">
-        <!-- will redirect to admin news -->
+        {{--
+            This is a list that hooks into material css so sub lists act as drop downs
+            when the main list item container is clicked
+         --}}
         <li>
             <div class="collapsible-header"><i class="material-icons"></i>News</div>
             <div class="collapsible-body">
-                <ul>
+                <ul> {{-- news drop down options--}}
                     <li class="collection-item"><a href="../api/news">Add News</a></li>
                     <li class="collection-item"><a href="../api/news">List News</a></li>
                 </ul>
@@ -16,7 +19,7 @@
         <li>
             <div class="collapsible-header"><i class="material-icons"></i>Events</div>
             <div class="collapsible-body">
-                <ul>
+                <ul>{{-- Events drop down options--}}
                     <li class="collection-item"><a href="../api/events">Add Event</a></li>
                     <li class="collection-item"><a href="../api/events">List Events</a></li>
                 </ul>
@@ -25,30 +28,30 @@
         <li>
             <div class="collapsible-header"><i class="material-icons"></i>Registration</div>
             <div class="collapsible-body">
-                <ul>
+                <ul>{{-- Registration options drop down options--}}
                     <li class="collection-item"><a href="../api/news">option1</a></li>
                     <li class="collection-item"><a href="../api/news">option2</a></li>
                 </ul>
             </div>
-        </li> <!-- will redirect to admin registration -->
+        </li>
         <li>
             <div class="collapsible-header"><i class="material-icons"></i>Settings</div>
             <div class="collapsible-body">
-                <ul>
+                <ul>{{-- Settings drop down options--}}
                     <li class="collection-item"><a href="../api/news">option1</a></li>
                     <li class="collection-item"><a href="../api/news">option2</a></li>
                 </ul>
             </div>
-        </li><!-- will redirect to admin news -->
+        </li>
         <li>
             <div class="collapsible-header"><i class="material-icons"></i>Accounts</div>
             <div class="collapsible-body">
-                <ul>
+                <ul>{{-- Accounts drop down options--}}
                     <li class="collection-item"><a href="../api/news">option1</a></li>
                     <li class="collection-item"><a href="../api/news">option2</a></li>
                     <li class="collection-item"><a href="../api/news">option3</a></li>
                 </ul>
             </div>
-        </li><!-- will redirect to admin news -->
+        </li>
     </ul>
 </div>
